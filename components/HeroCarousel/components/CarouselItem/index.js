574139f4f178279f67e0de24d947fe59f657e935
Vivian Yu <>
2020-08-21 11:04:12 -0700

    converted hero images to background images to allow for more image size flexibility

--- components/HeroCarousel/components/CarouselItem/index.js
+++ components/HeroCarousel/components/CarouselItem/index.js
@@ -5,10 +5,9 @@ export default function CarouselItem() {
   return (
     <div className={ComponentStyles.carousel_card_container}>
       <div className={ComponentStyles.carousel_card_container_inner}>
-        <img alt="" src="/assets/banner1.png" className={ComponentStyles.carousel_card_item_one} />
-
-        <img alt="" src="/assets/banner2.png" className={ComponentStyles.carousel_card_item_two} />
-      </div>
-    </div>
+        <div style={{ backgroundImage: "url(" + "/assets/banner1.png" + ")" }} className={ComponentStyles.carousel_card_item_one} ></div>
+        <div style={{ backgroundImage: "url(" + "/assets/banner2.png" + ")" }} className={ComponentStyles.carousel_card_item_two} ></div>
+      </div >
+    </div >
   );
 }
