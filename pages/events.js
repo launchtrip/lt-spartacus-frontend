0db7f847f0a3f05edcb808d659c59bb29ad691bc
David Khayutin <davidkhayutin@Davids-MacBook-Pro-2.local>
2020-09-10 17:19:01 -0700

    events page

--- pages/events.js
+++ pages/events.js
@@ -103,6 +103,6 @@ export const getServerSideProps = async () => {
     const results = organizeEventsByMonth(events);
     return { props: { data: results } };
   } catch (error) {
-    return { props: { data: undefined } };
+    return { props: { data: null } };
   }
 };
