b57f4e34b69e66d9bad2a78d02d62d85f93f0a76
Vivian Yu <>
2020-09-18 15:41:53 -0700

    rename News & Articles

--- components/EventPage/components/regularNews.js
+++ components/EventPage/components/regularNews.js
@@ -5,7 +5,7 @@ import NewsCard from '../../NewsCard';
 export default function RegularNews({ event }) {
   return (
     <section className={ComponentStyles.event_page_section_two_news}>
-      <span className={`typography_spartacus_eight ${ComponentStyles.event_page_section_two_news_title}`}>News & Opinion</span>
+      <span className={`typography_spartacus_eight ${ComponentStyles.event_page_section_two_news_title}`}>News & Articles</span>
       {event.articles.map((article) => <NewsCard alternate line article={article} />)}
     </section>
   );
