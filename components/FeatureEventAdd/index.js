f121b1a2a4b5d06aedc5ed4b535a1995d68d372a
David Khayutin <davidkhayutin@Davids-MacBook-Pro-2.local>
2020-09-29 11:49:15 -0700

    added link to parntership

--- components/FeatureEventAdd/index.js
+++ components/FeatureEventAdd/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import Link from 'next/link';
+
 import ComponentStyles from './style/styles.module.css';
 
 export default function FeatureEventAd() {
@@ -9,7 +11,9 @@ export default function FeatureEventAd() {
       <p className={`${ComponentStyles.copy} typography_spartacus_four`}>We know travel and corporate events.
         We help events succeed and we are super duper awesome.
       </p>
-      <a href="#" className={`${ComponentStyles.button} button_small_styled`}>Submit Event</a>
+      <Link href="/partnerships">
+        <a href="partnerships" className={`${ComponentStyles.button} button_small_styled`}>Submit Event</a>
+      </Link>
     </div>
   );
 }
