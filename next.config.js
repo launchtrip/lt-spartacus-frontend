85533d9630b63c3e09aa4311fa470c6cc77f888d
Wissam <wissam@launchtrip.com>
2020-10-02 06:10:51 +0200

    remove dotenv call

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config();
-}
-
 const url = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
 const firebaseProjectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
 const firebaseApiKey = process.env.NEXT_PUBLIC_FIREBASE_APIKEY;
