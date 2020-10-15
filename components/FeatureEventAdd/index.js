ab60ee63e93943afa8efc22793c0f0b0578e3a69
David Khayutin <davidkhayutin@Davids-MacBook-Pro-2.local>
2020-10-15 14:50:39 -0700

    updated add

--- components/FeatureEventAdd/index.js
+++ components/FeatureEventAdd/index.js
@@ -7,9 +7,9 @@ import ComponentStyles from './style/styles.module.css';
 export default function FeatureEventAd() {
   return (
     <div className={ComponentStyles.eventAd}>
-      <h1 className={`${ComponentStyles.title} typography_spartacus_eight`}>Join the assembly of the world&apos;s industry leading events.</h1>
-      <p className={`${ComponentStyles.copy} typography_spartacus_four`}>We know travel and corporate events.
-        We help events succeed and we are super duper awesome.
+      <h1 className={`${ComponentStyles.title} typography_spartacus_eight`}>Feature Your Event</h1>
+      <p className={`${ComponentStyles.copy} typography_spartacus_four`}>
+        Join the assembly of the world&apos;s industry leading events.
       </p>
       <Link href="/partnerships">
         <a href="partnerships" className={`${ComponentStyles.button} button_small_styled`}>Submit Event</a>
