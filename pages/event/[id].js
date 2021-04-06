066eec2397bee0ab4c2610de7c1729adc4d73af3
Jose Valdvogel <jose@launchtrip.com>
2021-04-06 15:30:01 -0700

    events

--- pages/event/[id].js
+++ pages/event/[id].js
@@ -29,6 +29,7 @@ export default function Events({ data }) {
 }
 
 export const getServerSideProps = async (props) => {
+  syncDMP(props, true);
   const id = props.req.url.replace('?', 'Q').split('-id-')[1];
   if (!id) {
     return { props: { data: null } };
